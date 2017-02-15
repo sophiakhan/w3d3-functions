b0dd479271bb379e61200cf4f38f2f5ecf96ac0d
Sophia Khan <sophiaxkhan@gmail.com>
2017-02-15 14:58:56 -0500

    Created calculator fuctions

--- calc.js
+++ calc.js
@@ -4,12 +4,31 @@ var answer = 0;
 
 // 1. Write a function called add that takes two arguments and adds them together, returning the result
 
+function add (number1, number2) {
+
+    return number1 + number2;
+}
+
 // 2. Write a function called subtract that takes two arguments and subtracts them from each other, returning the result
+
+function subtract(number1, number2) {
+
+    return number1 - number2;
+}
 
 // 3. Write a function called multiply that takes two arguments and multiplies them other, returning the result
 
+function multiply(number1, number2) {
+
+    return number1 * number2;
+}
+
 // 4. Write a function called divide that takes two arguments and divides them from each other, returning the result
 
+function divide(number1, number2) {
+
+    return number1 / number2;
+}
 
 
 // Event Handlers
